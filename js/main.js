9a4015386a44acf27003b6628ef9c735a4305a42
nsudekenechi-tech <nsudekenechi2@gmail.com>
2021-03-23 11:29:53 +0100

    Update
    Responsiveness

---
+++ js/main.js
@@ -0,0 +1,106 @@
+const contact=document.querySelector('#contact');
+    const dashboard=document.querySelector('#dashboard');
+    const profile=document.querySelector('#profile');
+    const settings=document.querySelector('#settings');
+    const round1=document.querySelectorAll('.round1');
+    const round=document.querySelectorAll('.round');
+    const classdashboard=document.querySelector('.dashboard');
+    const classcontact=document.querySelector('.contact');
+    const classprofile=document.querySelector('.profile');
+    const classsettings=document.querySelector('.settings');
+    const add=document.querySelector('.add');
+    const hidden=document.querySelector('.hidden');
+
+    classcontact.addEventListener('click',()=>{
+        round.forEach((roun)=>{
+            roun.classList.remove('round');
+            roun.classList.add('round1');
+        })
+        round1[1].classList.remove('round');
+        round1[1].classList.add('round1');
+
+        round1[2].classList.remove('round');
+        round1[2].classList.add('round1');
+
+        dashboard.style.display='none';
+        profile.style.display='none';
+        settings.style.display='none';
+
+        contact.style.display='block';
+        round1[0].classList.add('round');
+        round1[0].classList.remove('round1');
+        
+    })
+    classprofile.addEventListener('click',()=>{
+        round.forEach((roun)=>{
+            roun.classList.remove('round');
+            roun.classList.add('round1');
+        })
+        round1[0].classList.remove('round');
+        round1[0].classList.add('round1');
+
+        round1[2].classList.remove('round');
+        round1[2].classList.add('round1');
+
+        dashboard.style.display='none';
+        contact.style.display='none';
+        profile.style.display='block';
+        settings.style.display='none';
+        round1[1].classList.add('round');
+        round1[1].classList.remove('round1');
+
+    })
+    classsettings.addEventListener('click',()=>{
+        round.forEach((roun)=>{
+            roun.classList.remove('round');
+            roun.classList.add('round1');
+        })
+        round1[0].classList.remove('round');
+        round1[0].classList.add('round1');
+        round1[1].classList.remove('round');
+        round1[1].classList.add('round1');
+
+        dashboard.style.display='none';
+        contact.style.display='none';
+        profile.style.display='none';
+        settings.style.display='block';
+        round1[2].classList.add('round');
+        round1[2].classList.remove('round1');
+
+    })
+    add.addEventListener('click',()=>{
+        hidden.click();
+    })
+
+    
+
+    var events = [
+    {'Date': new Date(2016, 6, 7), 'Title': 'Doctor appointment at 3:25pm.'},
+    {'Date': new Date(2016, 6, 18), 'Title': 'New Garfield movie comes out!', 'Link': 'https://garfield.com'},
+    {'Date': new Date(2016, 6, 27), 'Title': '25 year anniversary', 'Link': 'https://www.google.com.au/#q=anniversary+gifts'},
+];
+
+    var element = document.getElementById('caleandar');
+caleandar(element, events, settings)
+
+const viewrecords=document.querySelector('.viewrecords');
+const month=document.querySelector('.mont');
+const day=document.querySelector('.da');
+const year=document.querySelector('.yea');
+let bool=false;
+viewrecords.addEventListener('click',()=>{
+    if(bool==false){
+        day.style.transform='translatex(-100%)'
+        month.style.transform='translatey(-110%)'
+        bool=!bool;
+
+    }else{
+        month.style.transform='translatex(-100%)';
+        year.style.transform='translatey(-220%)';
+        
+    }
+   
+    // day.style.display='none';
+    // month.style.display='block';
+    
+})
