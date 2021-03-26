754e27e91e954a69d1737609b50c274fa21bf796
godson-pius <40927597+godson-pius@users.noreply.github.com>
2021-03-26 09:05:38 +0100

    Fixed the student dashboard

--- student/index.php
+++ student/index.php
@@ -4,16 +4,16 @@
     <meta charset="UTF-8">
 
     <title>APTECH</title>
-    <link rel="stylesheet" href="css/Bootstrap/css/bootstrap.min.css">
-    <link rel="stylesheet" href="css/style1.css">
-    <script src="css/Bootstrap/js/jquery-3.5.1.js"></script>
+    <link rel="stylesheet" href="../css/Bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="../css/style1.css">
+    <script src="../css/Bootstrap/js/jquery-3.5.1.js"></script>
 </head>
 <body>
 <div class="container">
 
     <nav class="navbar navbar-expand-lg navbar-light bg-light">
         <a class="navbar-brand" href="#">
-            <img  src="img/aptechlogo.jpg" style="height: 3rem;" class=" bordaraduis card-img-top mt-2 mb-3 text-center">
+            <img  src="../img/aptechlogo.jpg" style="height: 3rem;" class=" bordaraduis card-img-top mt-2 mb-3 text-center">
 
 
         </a>
@@ -61,7 +61,7 @@
 
         <div class="col-lg-4 col-md-4 mt-5">
 
-            <form class="form-inline  my-2 my-lg-0">
+            <form class="form-inline my-2 my-lg-0">
                 <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
                 <button class="btn btn-outline-primary my-2 my-sm-0" type="submit">Search</button>
             </form>
@@ -77,33 +77,33 @@
             <div class="modal-content">
                 <div class="modal-header">
                     <h5 class="modal-title" id="exampleModalScrollableTitle">
-                        <img  src="img/aptechlogo.jpg" style="width: 5rem; height: 2rem;" class=" bordaraduis card-img-top mt-2 mb-3 text-center">
+                        <img  src="../img/aptechlogo.jpg" style="width: 5rem; height: 2rem;" class=" bordaraduis card-img-top mt-2 mb-3 text-center">
                     </h5>
                     <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                         <span aria-hidden="true">&times;</span>
                     </button>
                 </div>
                 <div class="modal-body">
-                    <form method="POST" action="process.php" name="login_form">
+                    <form action="process.php" method="POST" name="login_form">
                         <div class="form-group">
                             <label for="exampleInputEmail1">Student ID</label>
-                            <input type="text" class="form-control" name="email" id="exampleInputEmail1" aria-describedby="emailHelp">
+                            <input type="text" class="form-control" name="email" aria-describedby="emailHelp">
                             <small id="emailHelp" class="form-text text-muted">Please do not share your Student Id with anyone else.</small>
                         </div>
                         <div class="form-group">
                             <label for="exampleInputPassword1">Password</label>
-                            <input type="password" name="password" class="form-control" id="exampleInputPassword1" required>
+                            <input type="password" name="password" class="form-control" required>
                             <span class="text-warning"><a href="#">Forgot Password?</a></span>
                         </div>
                         <div class="form-group form-check">
-                            <input type="checkbox" name="check" class="form-check-input" id="exampleCheck1" >
+                            <input type="checkbox" name="check" class="form-check-input">
                             <label class="form-check-label" for="exampleCheck1">Proceed to Profile</label>
                         </div>
-                       <input type="submit" name="submit" id="signInSignOut" class="btn btn-primary" data-dismiss="modal" value="Sign In" >
+                       <input type="submit" name="submit" class="btn btn-primary" value="Sign In">
                     </form>
                 </div>
                 <div class="modal-footer">
-<!--                   <button type="button" class="btn btn-primary">Login</button>-->
+                    <!-- <button type="button" class="btn btn-primary">Login</button> -->
                 </div>
             </div>
         </div>
@@ -113,6 +113,15 @@
 </div>
 
 </body>
-<script src="css/Bootstrap/js/jquery-3.5.1.min.js"></script>
-<script src="css/Bootstrap/js/bootstrap.min.js"></script>
+<script src="../css/Bootstrap/js/jquery-3.5.1.min.js"></script>
+<script src="../css/Bootstrap/js/bootstrap.min.js"></script>
+
+<?php
+    if (isset($_GET['err'])) {
+        $err = $_GET['err'];
+        echo "<script>
+            alert('$err')
+        </script>";
+    }
+?>
 </html>
--- student/process.php
+++ student/process.php
@@ -1,24 +1,23 @@
 <?php
-require_once("config.php");
+	require_once 'config.php';
 
+	if (isset($_POST['submit'])) {
+		$email = $_POST['email'];
+		$tmp_password = $_POST['password'];
+		$password = sha1($tmp_password);
+		$check = $_POST['check'];
 
+		$sql = "SELECT * FROM students WHERE Email_address = '$email' AND Password = '$password'";
+		$query = mysqli_query($link, $sql);
 
-    $email = $_POST['email'];
-    $pass = $_POST['password'];
-    $check = $_POST['check'];
-
-
-    $query ="SELECT * FROM students WHERE Email_address = '$email' && Password = '$pass'";
-    $result = mysqli_query($link,$query);
-    if($result){
-        
-        header("location:dashboard.php"); 
-    } 
-    
-    else{
-
-        header("Location: index.php");    
-    }
+		if (mysqli_num_rows($query) > 0) {
+			$data = mysqli_fetch_assoc($query);
+			$name = $data['First_name'];
+			header("Location: dashboard.html?name=$name");
+		} else {
+			header("Location: index.php?err=Not Valid");
+		}
+	}
 
 
 
