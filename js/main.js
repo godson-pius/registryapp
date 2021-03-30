b9fd5320341fc5468edb180147bba09376188a1c
nsudekenechi-tech <nsudekenechi2@gmail.com>
2021-03-30 12:10:57 +0100

    update

--- js/main.js
+++ js/main.js
@@ -95,14 +95,7 @@ const contact=document.querySelector('#contact');
 
     })
     
-    var events = [
-    {'Date': new Date(2016, 6, 7), 'Title': 'Doctor appointment at 3:25pm.'},
-    {'Date': new Date(2016, 6, 18), 'Title': 'New Garfield movie comes out!', 'Link': 'https://garfield.com'},
-    {'Date': new Date(2016, 6, 27), 'Title': '25 year anniversary', 'Link': 'https://www.google.com.au/#q=anniversary+gifts'},
-];
-
-    var element = document.getElementById('caleandar');
-caleandar(element, events, settings)
+ 
 
 const viewrecords=document.querySelector('.viewrecords');
 const month=document.querySelector('.mont');
